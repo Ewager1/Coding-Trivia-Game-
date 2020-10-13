34c8ad97c771e6fb0afbeafba162ac438ff77a13
EWager <ethanbwager@gmail.com>
2020-10-12 20:16:20 -0400

    created questions section in HTML and set buttons up for answers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+/* next steps
+1. Change questions html to have 4 buttons that sit on top of eachother
+2. write onclick logic that replaces header, question, and button text with  next index. 
+3. On start click, make a timer start
+4. Create correct and incorrect boolean
+    a. assign boolean classes to buttons
+    b. make wrong booleans subtract time from timer
+    c.create small popup that says correct or incorrect with boolean 
+5. write out ending screen in html
+    a. create logic for timer=0 to jump to ending screen
+6. style 
+    a. title page
+    b. questions
+    c.high scores page
+    d.add sound effects
+    e. add favicon 
+    
+
+
 /* assignment section */ 
 const button = document.querySelector('.button')
 const titleScreenJumbo = document.querySelector('.titleScreenJumbo')
