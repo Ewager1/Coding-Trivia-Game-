2a0dbc3f7f7bfea25e77b03b52170f1be276b878
EWager <ethanbwager@gmail.com>
2020-10-15 17:02:55 -0400

    created logic for adding name and score to page. added quiz End button to reset storage and to go back to beginning of quiz

---
+++ assets/js/highScores.js
@@ -0,0 +1,34 @@
+//for appending to jumbotron
+const highScoresJumbo =document.querySelector('.highScoresJumbo')
+const list = document.createElement('ul');
+//the user name entered as an array
+const names = Object.keys(localStorage)
+//the user's score as an array 
+const scores = Object.values(localStorage)
+//goBack button 
+const goBack = document.querySelector('.goBack')
+const resetScores = document.querySelector('.resetScores')
+//creates a list item dynamicaly and appends list item to list
+
+for (i=0; i<names.length; i++){
+    let li = document.createElement('li')
+    li.setAttribute('class', 'grabToReset')
+    li.innerHTML = names[i] + "'s score:     " + scores[i];
+    list.appendChild(li);
+}
+
+//appends list to page 
+highScoresJumbo.appendChild(list)
+
+//brings user to title screen
+goBack.addEventListener('click', function(){
+    window.location.href = "/index.html"
+})
+
+resetScores.addEventListener('click', function(){
+    localStorage.clear();
+    resetText = document.querySelectorAll('.grabToReset')
+    for(i=0; i<3; i++)
+    resetText[i].innerHTML = ''
+    
+})
